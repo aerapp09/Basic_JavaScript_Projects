22ca1cd579c2be1677df4532cde77e074848a540
aerapp09 <126913232+aerapp09@users.noreply.github.com>
2023-03-20 15:00:26 -0700

    Index html

---
+++ basic_javascript/Project2_functions/main.js
@@ -0,0 +1,6 @@
+
+function myFunction() {
+    var sentence = "I am learning"; //establish the first variable
+    sentence += " a lot from this boo!"; //concatenates the string
+    document.getElementById("Concatenate").innerHTML = sentence; //displays the concatenated string
+}
